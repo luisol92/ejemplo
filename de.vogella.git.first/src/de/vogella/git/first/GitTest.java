a48ac8026be03d97fb001ee3d0e84e92ea183162
Olmedo <luisol_92@hotmail.com>
2012-11-06 00:03:27 -0500

    listo

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -3,7 +3,7 @@ package de.vogella.git.first;
 public class GitTest {
 	
 	public static void main(String[] args) {
-		System.out.println("Git  es  Divertido");
+		System.out.println("La  Programacion es Genial");
 
 	}
 
